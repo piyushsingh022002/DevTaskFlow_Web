6d70f9d08d019b6bfdd21e0918b61440c7a3d653
Piyush <piyushsingh6521@gmail.com>
2025-12-16 17:06:33 +0530

    Theme Added

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import { AuthProvider } from './providers/AuthProvider';
+import { ThemeProvider } from './context/ThemeContext';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider>
     <BrowserRouter>
     <AuthProvider>
       <AppRoutes />
     </AuthProvider>
     </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container, Card, Title, ButtonGroup, ActionButton } from "./Home.styles";
 import { useNavigate } from "react-router-dom";
+import { ThemeContext } from "../../context/ThemeContext";
 
 const Home: React.FC = () => {
 
     const navigate = useNavigate();
+    const {theme, toggleTheme} = useContext(ThemeContext);
   return (
-    <Container>
+    <Container className={theme}>
       <Card>
         <Title>Welcome Abroad</Title>
 
         <ButtonGroup>
           <ActionButton variant="primary" onClick={() => navigate("/login")}>Login</ActionButton>
           <ActionButton variant="secondary" onClick={() => navigate("/register")}>Register</ActionButton>
+          <ActionButton variant="primary" onClick={toggleTheme}>
+            Switch to {theme === "light" ? "Dark" : "Light"} Mode
+          </ActionButton>
         </ButtonGroup>
       </Card>
     </Container>
